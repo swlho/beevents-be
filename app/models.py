68bd31bef8986f9ab816e5d3718ce8da89147526
swlho <swlho87@gmail.com>
2024-10-01 13:46:43 +0800

    feat: add event model

--- app/models.py
+++ app/models.py
@@ -5,3 +5,16 @@ class User(BaseModel):
     last_name: str
     password: str
     email: str
+
+
+class Event(BaseModel):
+    title: str
+    date_time: str
+    details: str
+    location: str
+    map_data: str
+    tags: list
+    users_attending: list
+    is_archived: bool
+    cost: int
+    total_revenue: int
