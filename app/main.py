9a841ab244bcb36ed535f7a1aab638a9964a7b34
swlho <swlho87@gmail.com>
2024-10-01 13:46:16 +0800

    feat: add /events route

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from .routes.user_routes import router as UserRouter
+from .routes.events_routes import router as EventsRouter
 
 app = FastAPI()
 
-# Routes
+# ROUTES
+# User
 app.include_router(UserRouter, tags=['user'], prefix='/user')
+# Events
+app.include_router(EventsRouter, tags=['events'], prefix='/events')
