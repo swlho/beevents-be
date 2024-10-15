31ff47f73a6f745e393d6644d48c25008bf064df
swlho <swlho87@gmail.com>
2024-10-15 10:43:34 +0800

    feat: add get user by id

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,6 +1,6 @@
+from typing import Union
 from fastapi import APIRouter, Response, status
 from db.supabase import create_supabase_client
-from app.models import User
 #fn dependencies
 import bcrypt
 
@@ -11,35 +11,21 @@ supabase = create_supabase_client()
 router = APIRouter()
 
 def user_exists(key: str = "email", value: str = None):
-    user = supabase.from_("users").select("*").eq(key, value).execute()
+    user = supabase.from_("profiles").select("*").eq(key, value).execute()
     return len(user.data) > 0
 
-# POST NEW USER
-@router.post("/")
-def create_user(user: User, response: Response):
+@router.get("/{user_id}")
+def get_user_by_id(response: Response, user_id: Union[str, None] = None):
     try:
-        # Convert email to lowercase
-        user_email = user.email.lower()
-        # Hash password
-        hashed_password = bcrypt.hashpw(user.password.encode('utf-8'), bytes(bcrypt.gensalt()))
+        if user_id:
+            user = supabase.from_("profiles")\
+                .select("id", "updated_at", "full_name", "email", "events_attending", "archived_events")\
+                .eq("id", user_id)\
+                .execute()
 
-        # Check if user already exists
-        if user_exists(value=user_email):
-            response.status_code = status.HTTP_400_BAD_REQUEST
-            return {"message": "User already exists"}
-
-        # Add user to users table
-        user = supabase.from_("users")\
-            .insert({"first_name": user.first_name, "last_name": user.last_name, "password": str(hashed_password), "email": user_email, })\
-            .execute()
-
-        # Check if user was added
-        if user:
-            return {"message": "User created successfully"}
-        else:
-            response.status_code = status.HTTP_400_BAD_REQUEST
-            return {"message": "User creation failed"}
+            if user:
+                return user
     except Exception as e:
-        print("Error: ", e)
-        response.status_code = status.HTTP_400_BAD_REQUEST
-        return {"message": "User creation failed"}
+        print(f"Error: {e}")
+        response.status_code = status.HTTP_204_NO_CONTENT
+        return {"message": "User ID not found"}
