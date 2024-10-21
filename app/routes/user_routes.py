945c8f98e7f884938ecb250db9995380daadce8d
swlho <swlho87@gmail.com>
2024-10-21 17:52:06 +0800

    fix: add stripe create payment session

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,8 +1,13 @@
+import os
 from typing import Union
 from fastapi import APIRouter, Response, status
 from db.supabase import create_supabase_client
 #fn dependencies
 import bcrypt
+import stripe
+from config import stripe_api_key
+
+stripe.api_key = stripe_api_key
 
 # Initialize supabase client
 supabase = create_supabase_client()
@@ -32,29 +37,47 @@ def get_user_by_id(response: Response, user_id: Union[str, None] = None):
 
 #BOOK OR CANCEL EVENT BOOKING BY USER ID
 @router.patch("/{user_id}/events/{event_id}")
-def patch_book_event_by_id(user_id:str, event_id:int, response: Response, book: Union[bool, None] = None):
-    try:
-        events = supabase.from_("bookings")\
-        .select("event_id")\
-        .eq("user_id", user_id)\
-        .execute()
-        
-        if(events):
-            eventsArr = events.data
-            for event in eventsArr:
-                if(event_id is event["event_id"] and book is True):
-                    response.status_code = status.HTTP_400_BAD_REQUEST
-                    return {"message":"You are already booked into this event", "status code": response.status_code}
+def patch_book_event_by_id(user_id:str, event_id:int, response: Response, book: Union[bool, None] = None, cost: Union[int, None] = None):
+    if cost == 0:
+        try:
+            events = supabase.from_("bookings")\
+            .select("event_id")\
+            .eq("user_id", user_id)\
+            .execute()
             
-            if(book is True):
-                data = supabase.from_("bookings")\
-                .insert({"event_id":event_id, "user_id": user_id})\
-                .execute()
-
-                if(response):
-                    response.status_code = status.HTTP_200_OK
-                    return {"message":"Booking successful", "status code": response.status_code}
+            if(events):
+                eventsArr = events.data
+                for event in eventsArr:
+                    if(event_id is event["event_id"] and book is True):
+                        response.status_code = status.HTTP_400_BAD_REQUEST
+                        return {"message":"You are already booked into this event", "status code": response.status_code}
                 
+                if(book is True):
+                    data = supabase.from_("bookings")\
+                    .insert({"event_id":event_id, "user_id": user_id})\
+                    .execute()
+
+                    if(response):
+                        response.status_code = status.HTTP_200_OK
+                        return {"message":"Booking successful", "status code": response.status_code}
+                    
+                if(book is False):
+                    data = supabase.from_("bookings")\
+                    .delete()\
+                    .eq("user_id", user_id)\
+                    .eq("event_id", event_id)\
+                    .execute()
+                    if(response):
+                        response.status_code = status.HTTP_200_OK
+                        return {"message":"Booking cancellation successful", "status code": response.status_code}
+                        
+        except Exception as e:
+            print(f"Error: {e}")
+            response.status_code = status.HTTP_400_BAD_REQUEST
+            return {"message": "Something happened. Please try again"}
+    
+    if cost > 0:
+        try:
             if(book is False):
                 data = supabase.from_("bookings")\
                 .delete()\
@@ -64,11 +87,50 @@ def patch_book_event_by_id(user_id:str, event_id:int, response: Response, book: 
                 if(response):
                     response.status_code = status.HTTP_200_OK
                     return {"message":"Booking cancellation successful", "status code": response.status_code}
+            
+            if(book is True):
+                events = supabase.from_("bookings")\
+                .select("event_id")\
+                .eq("user_id", user_id)\
+                .execute()
+            
+                if(events):
+                    eventsArr = events.data
+                    for event in eventsArr:
+                        if(event_id is event["event_id"] and book is True):
+                            response.status_code = status.HTTP_400_BAD_REQUEST
+                            return {"message":"You are already booked into this event", "status code": response.status_code}
                     
-    except Exception as e:
-        print(f"Error: {e}")
-        response.status_code = status.HTTP_400_BAD_REQUEST
-        return {"message": "Booking cancellation failed"}
+                    price_id = supabase.from_("events")\
+                    .select("price_id")\
+                    .eq("event_id", event_id)\
+                    .execute()
+
+                    domain_url = "http://localhost:3000"
+                    patchPriceId = price_id.data[0]["price_id"]
+
+                    checkout_session = stripe.checkout.Session.create(
+                        line_items=[{
+                            'price': patchPriceId,
+                            'quantity': 1,
+                        }],
+                        metadata={
+                            "user_id": user_id,
+                            "event_id": event_id,
+                            "cost": cost,
+                        },
+                        mode='payment',
+                        success_url = domain_url +'/success.html',
+                        cancel_url = domain_url +'/cancel.html',
+                    )
+                    response.status_code = status.HTTP_303_SEE_OTHER
+                    return {"url":checkout_session.url, "status_code":response.status_code}
+            
+        except Exception as e:
+            print(f"Error: {e}")
+            response.status_code = status.HTTP_400_BAD_REQUEST
+            return {"message": "Something happened. Please try again"}
+        
 
 #DELETE FROM USER'S ARCHIVED EVENTS
 @router.delete("/{user_id}/archived-events/{event_id}")
