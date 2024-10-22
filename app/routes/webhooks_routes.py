073016ffb2a7641dbceb77a374752bc1c86b4871
swlho <swlho87@gmail.com>
2024-10-22 11:54:39 +0800

    fix: add event handler to make booking on db

--- app/routes/webhooks_routes.py
+++ app/routes/webhooks_routes.py
@@ -1,14 +1,20 @@
-from fastapi import APIRouter, FastAPI, Request, HTTPException, Header
+from fastapi import APIRouter, FastAPI, Request, status, HTTPException, Header, Response
 from config import webhook_secret
 import stripe
 
+from db.supabase import create_supabase_client
+
 router = APIRouter()
 webhook_secret: str = webhook_secret
+
+# Initialize supabase client
+supabase = create_supabase_client()
 
 @router.post("/checkout-complete")
 async def stripe_webhook(
     event: dict,
     request: Request,
+    response: Response,
     stripe_signature=Header(None),
 ):
     raw_body = await request.body()
@@ -25,6 +31,16 @@ async def stripe_webhook(
     data = event["data"]["object"]
     if event["type"] == "checkout.session.completed":
         # your custom function to run after a successful payment
-        print('Succesful checkout')
+
+        event_id = data.metadata["event_id"]
+        user_id = data.metadata["user_id"]
+
+        booking = supabase.from_("bookings")\
+        .insert({"event_id":event_id, "user_id": user_id, "paid": True})\
+        .execute()
+
+        if(booking):
+            response.status_code = status.HTTP_200_OK
+            return {"message":"Booking successful", "status code": response.status_code, "booking": booking}
 
     return 200
